237dc5235fd278caa1cb31dda42769d917a2e29f
Lintercloud <hsulinter@gmail.com>
2020-11-21 21:37:59 +0800

    add word analytics

--- reviews-analytics.py
+++ reviews-analytics.py
@@ -9,15 +9,44 @@ sum_line = 0
 with open  ("reviews.txt", "r") as f:
     for line in f:
         data.append(line)
+        if len(data) % 100000 == 0:
+            print("讀取了", len(data), "筆留言")
         
     print("總共有", len(data),"筆留言")
     
+#利用字典來個字數的統計
+wc = {} #word_count
 for d in data:
-    sum_line += len(d)
-      
+    words = d.split()
+    for word in words:
+        if word in wc:
+            wc[word] += 1    
+        else:
+            wc[word] = 1
+            
+#查詢字的出現次數
+while True:
+    word = input("請輸入查詢單字")
+    if word == "q":
+        break
+    if word in wc:
+        print(word, "這單字有",wc[word],"次")
+    else:
+        print("這字沒出現過喔")
+    
+for word in wc:
+    if wc[word] > 1000000:
+        print(word, wc[word])
+        
+
+
+for d in data:
+    sum_line += len(d)     
 print("平均留言長度為", int(sum_line / (len(data))), "個字")
 
+
 new = []
+#new = [d for d in data if len(d) < 100] 為下面的縮寫
 for d in data:
     if len(d) < 100:
         new.append(d)
@@ -28,3 +57,5 @@ for d in data:
     if "good" in d:
         good.append(d)
 print("Good的留言有", len(good), "筆")
+
+
